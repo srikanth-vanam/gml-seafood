b5cee1777c3b82972c8f3ad57e87bee5b12360a0
srikanth-vanam <B172320-@rgukt.ac.in>
2023-10-15 14:54:42 +0530

    resolved icons display, changed title and other modifications in about,home pages

--- src/components/contactPage/ContactPage.js
+++ src/components/contactPage/ContactPage.js
@@ -1,7 +1,7 @@
 import ContactFooter from "./footer/ContactFooter";
 import MoblileFooter from "../mobilefooter/MobileFooter";
 import Contact from "./contact/Contact";
-import ContactHero from "./home/ContactHero";
+import ContactHero from "./hero/ContactHero";
 import Header from "../header/Header";
 
 const ContactPage = () => {
